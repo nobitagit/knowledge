be5b202604c3c5b424f60e8dc239b9b90c082019
Aurelio Ogliari <aurelio.ogliari@gmail.com>
2019-12-03 02:12:12 +0100

    stats - aggregate changed files in an array

--- _repoUtils/main.go
+++ _repoUtils/main.go
@@ -5,8 +5,34 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
+
+func isMdFile(fileName []byte) bool {
+	return bytes.HasSuffix(fileName, []byte(".md")) || bytes.HasSuffix(fileName, []byte(".MD"))
+}
+
+func isBlackListed(fileName []byte) bool {
+	ignoredDirs := [2]string{".git", "_repoUtils"}
+
+	dir := filepath.Dir(string(fileName))
+
+	// top level files are always ignored, so we return early
+	if dir == "." {
+		return true
+	}
+	// derive the top level directory from the path
+	baseDir := strings.Split(dir, string(os.PathSeparator))[0]
+	var contained bool
+	for _, ignoredDir := range ignoredDirs {
+		contained = ignoredDir == baseDir
+	}
+
+	return contained
+}
 
 func main() {
 	cmd := exec.Command("git", "log", "--pretty=format:", "--name-only", "--since='30 days ago'", "--stat")
@@ -17,8 +43,15 @@ func main() {
 	}
 	r := bytes.NewReader(out)
 	scanner := bufio.NewScanner(r)
+
+	var filePaths [][]byte
 	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		filePath := scanner.Bytes()
+		isMd := isMdFile(filePath)
+		if isMd == true && !isBlackListed(filePath) {
+			filePaths = append(filePaths, filePath)
+			fmt.Printf("%v contains md\n", scanner.Text())
+		}
 	}
-	//	fmt.Printf("%v\n", string(out))
+	fmt.Printf("%s\n", filePaths)
 }
